8f817c4007e66d26cd491e2e44518dde1c6bbcdc
Austin Raney <austin.raney@noaa.gov>
2024-02-22 09:20:56 -0500

    test: lgar

--- python/ngen_conf/tests/test_init_config_models.py
+++ python/ngen_conf/tests/test_init_config_models.py
@@ -1,9 +1,10 @@
-from ngen.init_config import utils
 from ngen.config.init_config.cfe import CFE
-from ngen.config.init_config.pet import PET
+from ngen.config.init_config.lgar import Lgar
 from ngen.config.init_config.noahowp import NoahOWP
+from ngen.config.init_config.pet import PET
 from ngen.config.init_config.soil_freeze_thaw import SoilFreezeThaw
 from ngen.config.init_config.soil_moisture_profile import SoilMoistureProfile
+from ngen.init_config import utils
 
 
 def test_cfe(cfe_init_config: str):
@@ -33,3 +34,7 @@ def test_soil_freeze_thaw(soil_freeze_thaw_init_config: str):
 def test_soil_moisture_profile(soil_moisture_profile_init_config: str):
     o = SoilMoistureProfile.from_ini_str(soil_moisture_profile_init_config)
     assert o.to_ini_str() == soil_moisture_profile_init_config
+
+def test_lgar(lgar_init_config: str):
+    o = Lgar.from_ini_str(lgar_init_config)
+    assert o.to_ini_str() == lgar_init_config
