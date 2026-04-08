4ad53695a05cbf4fb64b7947f398fb8a8a6c8ecb
Soroush Sorourian <ssorourian@lynker.com>
2026-04-08 16:28:09 -0400

    Update SNOW17 schema to use explicit Field unit wrapper

--- python/ngen_conf/src/ngen/config/init_config/snow17.py
+++ python/ngen_conf/src/ngen/config/init_config/snow17.py
@@ -5,17 +5,14 @@ import typing
 from datetime import datetime
 
 import typing_extensions
-from pydantic import validator, Field
+from pydantic import validator
 
 from ngen.config.path_pair.path_pair import PathPair, path_pair
 from ngen.init_config import serializer_deserializer as serde
 from ngen.init_config.deserializer import from_namelist_str
 from ngen.init_config.serializer import format_serializers
-from ngen.init_config.units import (
-    Meter,
-    DimensionlessField,
-    Unit,
-)
+from ngen.init_config.units import CommonUnits, Field
+
 
 # TODO: refactor into independent module
 from .topmodel import _maybe_into_readliner, _Readliner
@@ -125,21 +122,21 @@ class _Snow17Wrapper(serde.NamelistSerializerDeserializer):
 # NOTE: this is not a general snow17 config file parser.
 #       it only parses snow17 config files usable in ngen
 class Snow17Params(serde.GenericSerializerDeserializer):
-    hru_area: float = Unit("km**2")  # sq-km, needed for combination & routing conv.
-    latitude: float= Unit("degree")  # centroid latitude of hru (decimal degrees)
-    elev: float = Meter  # mean elevation of hru (m)
-    scf: float = DimensionlessField(ge=0.0)
-    mfmax: float = Unit("mm / degC / hour")
-    mfmin: float = Unit("mm / degC / hour")
-    uadj: float = Unit("mm / mbar")
-    si: float = Unit("mm")
-    pxtemp: float = Unit("degC")
-    nmf: float = Unit("mm / degC / hour")
-    tipm: float = DimensionlessField(ge=0.0, le=1.0)
-    mbase: float = Unit("degC")
-    plwhc: float = DimensionlessField(ge=0.0, le=1.0)
-    daygm: float = Unit("mm / day")
-    adc: list[float] =  DimensionlessField(min_items=11, max_items=11)
+    hru_area: float = Field(units="km**2")  # sq-km, needed for combination & routing conv.
+    latitude: float= Field(units="degree")  # centroid latitude of hru (decimal degrees)
+    elev: float = Field(units=CommonUnits.Meter)  # mean elevation of hru (m)
+    scf: float = Field(ge=0.0, units=CommonUnits.Dimensionless)
+    mfmax: float = Field(units="mm / degC / hour")
+    mfmin: float = Field(units="mm / degC / hour")
+    uadj: float = Field(units="mm / mbar")
+    si: float = Field(units="mm")
+    pxtemp: float = Field(units="degC")
+    nmf: float = Field(units="mm / degC / hour")
+    tipm: float = Field(ge=0.0, le=1.0, units=CommonUnits.Dimensionless)
+    mbase: float = Field(units="degC")
+    plwhc: float = Field(ge=0.0, le=1.0, units=CommonUnits.Dimensionless)
+    daygm: float = Field(units="mm / day")
+    adc: list[float] = Field(min_items=11, max_items=11, units=CommonUnits.Dimensionless)
 
     @typing_extensions.override
     @classmethod
