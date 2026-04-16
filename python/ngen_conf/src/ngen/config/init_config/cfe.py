e1ec55d0718979cd719274a26c6e90a6462592a2
Austin Raney <austin.raney@noaa.gov>
2026-04-16 17:33:52 -0400

    feat(ngen.config.init_config): update cfe models to v2 compliance
    support for nash_cascade and giuh surface_runoff_scheme.
    
    public classes removed and replaced by private equivalents:
    CFESchaakeCoupledSoilMoisture
    CFEXinanjiangCoupledSoilMoisture
    CFESchaake
    CFEXinanjiang
    
    the follow existing fields are now aliased under new names when serialized:
    
    'nash_storage'                -> 'nash_storage_subsurface'
    'surface_partitioning_scheme' -> 'surface_water_partitioning_scheme'
    'aet_rootzone'                -> 'is_aet_rootzone'
    'sft_coupled'                 -> 'is_sft_coupled'
    'max_root_zone_layer'         -> 'max_rootzone_layer'

--- python/ngen_conf/src/ngen/config/init_config/cfe.py
+++ python/ngen_conf/src/ngen/config/init_config/cfe.py
@@ -10,6 +10,13 @@ from ngen.init_config.units import CommonUnits, Field
 if TYPE_CHECKING:
     from pydantic.typing import AbstractSetIntStr, DictStrAny, MappingIntStrAny
 
+def _serialize_list(l: list[float]) -> str:
+    return ",".join(map(lambda x: str(x), l))
+
+def _coerce_lists(cls: type[serde.IniSerializerDeserializer], value: list[str] | str) -> list[str]:
+    if isinstance(value, list):
+        return value
+    return [x.strip() for x in value.split(",")]
 
 class CFEBase(serde.IniSerializerDeserializer):
     forcing_file: Literal["BMI"] = "BMI"
@@ -44,6 +51,8 @@ class CFEBase(serde.IniSerializerDeserializer):
     # initial condition for soil reservoir - it is the water in the soil as a decimal fraction of
     # maximum soil water storage (smcmax * depth) for the initial timestep
     soil_storage: float = Field(units=CommonUnits.Dimensionless)  # 66.7%
+    # number of Nash lf reservoirs; ignored if storage values present
+    n_nash_subsurface: int = 2
     # number of Nash lf reservoirs (optional, defaults to 2, ignored if storage values present)
     k_nash: float = Field(units=CommonUnits.Dimensionless)
     # Nash Config param - primary reservoir
@@ -58,28 +67,21 @@ class CFEBase(serde.IniSerializerDeserializer):
     # prints various debug and bmi info
     verbosity: int = Field(0, gte=0, lte=3)
 
-    # direct runoff
-    surface_partitioning_scheme: Literal["Schaake", "Xinanjiang"]
-
-    @validator("nash_storage", "giuh_ordinates", pre=True)
-    def _coerce_lists(cls, value):
-        if isinstance(value, list):
-            return value
-        return [x.strip() for x in value.split(",")]
+    _coerce_lists = validator(
+        "nash_storage", "giuh_ordinates", pre=True, allow_reuse=True
+    )(_coerce_lists)
 
     class Config(serde.IniSerializerDeserializer.Config):
-        def _serialize_list(l: list[float]) -> str:
-            return ",".join(map(lambda x: str(x), l))
-
-        field_serializers = {
-            "nash_storage": _serialize_list,
-            "giuh_ordinates": _serialize_list,
-        }
+        # NOTE: this should handle how all list types are serialized for all subclasses
+        field_type_serializers = {list: _serialize_list}
 
         fields = {
             "cgw": {"alias": "Cgw"},
             "k_lf": {"alias": "K_lf"},
-            "k_nash": {"alias": "K_nash"},
+            "k_nash": {"alias": "K_nash_subsurface"},
+            "n_nash_subsurface": {"alias": "N_nash_subsurface"},
+            # changed: https://github.com/NOAA-OWP/cfe/commit/96ea8502d018ae26c2fa48d7ea745dcc82c70c0e
+            "nash_storage": {"alias": "nash_storage_subsurface"},
             "soil_params_depth": {"alias": "soil_params.depth"},
             "soil_params_b": {"alias": "soil_params.b"},
             "soil_params_mult": {"alias": "soil_params.mult"},
@@ -90,16 +92,44 @@ class CFEBase(serde.IniSerializerDeserializer):
             "soil_params_wltsmc": {"alias": "soil_params.wltsmc"},
             "soil_params_expon": {"alias": "soil_params.expon"},
             "soil_params_expon_secondary": {"alias": "soil_params.expon_secondary"},
+            "surface_partitioning_scheme": {
+                "alias": "surface_water_partitioning_scheme"
+            },
         }
 
 
-class CFESchaake(CFEBase):
-    surface_partitioning_scheme: Literal["Schaake"] = "Schaake"
+class _NashCascadeSurfaceRunoffScheme(serde.IniSerializerDeserializer):
+    surface_runoff_scheme: Literal["NASH_CASCADE", "nash_cascade"]
+    n_nash_surface: int
+    k_nash_surface: float = Field(units="1/hour")
+    nsubsteps_nash_surface: int = 10  # default
+    nash_storage_surface: List[float] = Field(units=CommonUnits.Meter)
+    kinf_nash_surface: float = Field(default=0.001, units="1/hour")
+    retention_depth_nash_surface: float = Field(default=0.001, units=CommonUnits.Meter)
+
+    _coerce_lists = validator(
+        "nash_storage_surface", pre=True, allow_reuse=True
+    )(_coerce_lists)
+
+    class Config(serde.IniSerializerDeserializer.Config):
+        fields = {
+            "n_nash_surface": {"alias": "N_nash_surface"},
+            "k_nash_surface": {"alias": "K_nash_surface"},
+            "kinf_nash_surface": {"alias": "Kinf_nash_surface"},
+        }
 
 
-class CFEXinanjiang(CFEBase):
+class _GiuhSurfaceRunoffScheme(serde.IniSerializerDeserializer):
+    surface_runoff_scheme: Literal["GIUH", "giuh"] = "GIUH"
+
+
+class _Schaake(serde.IniSerializerDeserializer):
+    surface_partitioning_scheme: Literal["Schaake"]
+
+
+class _Xinanjiang(serde.IniSerializerDeserializer):
     # direct runoff
-    surface_partitioning_scheme: Literal["Xinanjiang"] = "Xinanjiang"
+    surface_partitioning_scheme: Literal["Xinanjiang"]
     a_xinanjiang_inflection_point_parameter: float
     b_xinanjiang_shape_parameter: float
     x_xinanjiang_shape_parameter: float
@@ -115,32 +145,61 @@ class CFEXinanjiang(CFEBase):
         }
 
 
-class CFESchaakeCoupledSoilMoisture(CFESchaake):
+class _CoupledSoilMoisture(serde.IniSerializerDeserializer):
     aet_rootzone: bool  # True, true, 1
     # layer of the soil that is the maximum root zone depth. That is, the depth of the layer where the AET is drawn from
     max_root_zone_layer: float = Field(units=CommonUnits.Meter)
     # an array of depths from the surface. Example, soil_layer_depths=0.1,0.4,1.0,2.0
-    soil_layer_depths: List[float]
+    soil_layer_depths: List[float] = Field(units=CommonUnits.Meter)
     # `ice-fraction based runoff` | when `CFE coupled to SoilFreezeThaw`
-    sft_coupled: bool  # True, true, 1
+    sft_coupled: bool = False # True, true, 1
+
+    _coerce_lists = validator(
+        "soil_layer_depths", pre=True, allow_reuse=True
+    )(_coerce_lists)
+
+    class Config(serde.IniSerializerDeserializer.Config):
+        fields = {
+            # changed in: https://github.com/NOAA-OWP/cfe/pull/105
+            "aet_rootzone": {
+                "alias": "is_aet_rootzone",
+            },
+            # changed in: https://github.com/NOAA-OWP/cfe/pull/105
+            "sft_coupled": {
+                "alias": "is_sft_coupled",
+            },
+            # changed in: https://github.com/NOAA-OWP/cfe/pull/105
+            "max_root_zone_layer": {
+                "alias": "max_rootzone_layer",
+            },
+        }
 
 
-class CFEXinanjiangCoupledSoilMoisture(CFEXinanjiang):
-    aet_rootzone: bool  # True, true, 1
-    # layer of the soil that is the maximum root zone depth. That is, the depth of the layer where the AET is drawn from
-    max_root_zone_layer: float = Field(units=CommonUnits.Meter)
-    # an array of depths from the surface. Example, soil_layer_depths=0.1,0.4,1.0,2.0
-    soil_layer_depths: List[float]
-    # `ice-fraction based runoff` | when `CFE coupled to SoilFreezeThaw`
-    sft_coupled: bool  # True, true, 1
+class _Giuh(_GiuhSurfaceRunoffScheme, CFEBase): ...
+class _Nash(_NashCascadeSurfaceRunoffScheme, CFEBase): ...
 
+# not coupled
+class _SchaakeGiuh(_Schaake, _Giuh): ...
+class _XinanjiangGiuh(_Xinanjiang, _Giuh): ...
+class _SchaakeNash(_Schaake, _Nash): ...
+class _XinanjiangNash(_Xinanjiang, _Nash): ...
+
+# coupled
+class _SchaakeGiuhCoupledSoilMoisture(_CoupledSoilMoisture, _SchaakeGiuh): ...
+class _XinanjiangGiuhCoupledSoilMoisture(_CoupledSoilMoisture, _XinanjiangGiuh): ...
+class _SchaakeNashCoupledSoilMoisture(_CoupledSoilMoisture, _SchaakeNash): ...
+class _XinanjiangNashCoupledSoilMoisture(_CoupledSoilMoisture, _XinanjiangNash): ...
 
 class CFE(serde.IniSerializerDeserializer):
     __root__: Union[
-        CFESchaakeCoupledSoilMoisture,
-        CFEXinanjiangCoupledSoilMoisture,
-        CFESchaake,
-        CFEXinanjiang,
+        _SchaakeGiuhCoupledSoilMoisture,
+        _XinanjiangGiuhCoupledSoilMoisture,
+        _SchaakeNashCoupledSoilMoisture,
+        _XinanjiangNashCoupledSoilMoisture,
+        _SchaakeGiuh,
+        _XinanjiangGiuh,
+        _SchaakeNash,
+        _XinanjiangNash,
     ]
 
     class Config(serde.IniSerializerDeserializer.Config):
