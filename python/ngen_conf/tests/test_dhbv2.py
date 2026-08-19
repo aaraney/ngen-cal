019b37549955137d537a6359aa421db587697ed5
seyounger <seth.e.younger@gmail.com>
2026-08-19 14:55:43 -0400

    Add test coverage for dhbv2

---
+++ python/ngen_conf/tests/test_dhbv2.py
@@ -0,0 +1,24 @@
+from ngen.config.formulation import Formulation
+from ngen.config.dhbv2 import Dhbv2Mts
+
+
+def test_init(dhbv2_params):
+    Dhbv2Mts(**dhbv2_params)
+
+
+def test_no_lib(dhbv2_params):
+    dhbv2 = Dhbv2Mts(**dhbv2_params)
+    assert "library" not in dhbv2.dict().keys()
+
+
+def test_dhbv2_formulation(dhbv2_params):
+    dhbv2 = Dhbv2Mts(**dhbv2_params)
+    f = {"params": dhbv2, "name": "bmi_python"}
+    dhbv2_formulation = Formulation(**f)
+    _dhbv2 = dhbv2_formulation.params
+    assert _dhbv2.name == "bmi_python"
+    assert _dhbv2.model_name == "dhbv2.0 mts"
+    serialized = _dhbv2.dict(by_alias=True)
+    assert serialized["model_type_name"] == "dhbv2.0 mts"
+    assert serialized["python_type"] == "dhbv2.mts_bmi.MtsDeltaModelBmi"
+    assert serialized["main_output_variable"] == "land_surface_water__runoff_volume_flux"
--- python/ngen_conf/tests/test_init_config_models.py
+++ python/ngen_conf/tests/test_init_config_models.py
@@ -7,6 +7,7 @@ from ngen.init_config import utils
 
 from ngen.config.init_config.cfe import CFE
 from ngen.config.init_config.casam import Casam
+from ngen.config.init_config.dhbv2 import Dhbv2Mts
 from ngen.config.init_config.noahowp import NoahOWP
 from ngen.config.init_config.pet import PET
 from ngen.config.init_config.soil_freeze_thaw import SoilFreezeThaw
@@ -127,6 +128,12 @@ def test_soil_moisture_profile(soil_moisture_profile_init_config: str):
 def test_casam(casam_init_config: str):
     o = Casam.from_ini_str(casam_init_config)
     assert o.to_ini_str() == casam_init_config
+
+
+def test_dhbv2(dhbv2_init_config: str):
+    o = Dhbv2Mts.from_yaml_str(dhbv2_init_config)
+    assert o.to_yaml_str() == dhbv2_init_config
+    assert o.catchment_id == "cat-27"
 
 
 def test_topmodel_subcat(topmodel_subcat_config: str):
